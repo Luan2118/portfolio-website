716fe97443afffd5d2713ac1b7adf3f781f12f72
Luan <leluanvn21@seznam.cz>
2026-06-09 20:56:08 +0200

    feat: add contact section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Projects from './components/sections/Projects'
 import './App.css'
 import Skills from './components/sections/Skills'
 import About from './components/sections/About'
+import Contact from './components/sections/Contact'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <Projects />
     <Skills />
     <About />
+    <Contact />
    </div>
   )
 }
---
+++ src/components/sections/Contact.tsx
@@ -0,0 +1,37 @@
+import Container from "../layout/Container";
+import Section from "../layout/Section";
+
+function Contact() {
+  return (
+    <Section id="contact">
+      <Container>
+        <div className="rounded-2xl border border-neutral-200 bg-neutral-50 p-8 shadow-sm md:p-10">
+          <div>
+            <h2 className="text-2xl font-semibold md:text-3xl text-neutral-950">
+              Contact
+            </h2>
+
+            <p className="mt-5 text-lg leading-8 text-neutral-600 max-w-2xl">
+              I am open to junior frontend developer opportunities. You can
+              contact me through email, GitHub, or LinkedIn.
+            </p>
+          </div>
+
+          <div className="mt-8 flex flex-col gap-3 sm:flex-row">
+            <a target="_blank" href="mailto:leluanvn21@seznam.cz"  className="inline-flex justify-center items-center bg-neutral-950 text-white text-sm rounded-lg py-2 px-4 font-semibold transition hover:bg-neutral-800 sm:py-3 sm:px-5">Email me</a>
+
+            <a target="_blank" href="https://github.com/Luan2118" className="inline-flex justify-center items-center border border-neutral-300 text-sm rounded-lg py-2 px-4 font-semibold transition hover:border-neutral-700 sm:py-3 sm:px-5">
+              GitHub
+            </a>
+
+            <a target="_blank" href="https://www.linkedin.com/in/luan-le-7671b9342/" className="inline-flex justify-center items-center border border-neutral-300 text-sm rounded-lg py-2 px-4 font-semibold transition hover:border-neutral-700 sm:py-3 sm:px-5">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </Container>
+    </Section>
+  );
+}
+
+export default Contact;
