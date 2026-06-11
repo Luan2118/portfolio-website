4e8b1a7d8554123de6e1032a67b899d65fadd7bb
Luan <leluanvn21@seznam.cz>
2026-06-11 20:04:43 +0200

    fix: typo style: adjust keyboard focus style

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -14,8 +14,8 @@ function Hero() {
           </p>
 
           <div className="flex flex-col gap-3 sm:flex-row mt-6">
-            <a href="#projects" className="inline-flex justify-center items-center bg-neutral-950 text-white text-sm rounded-lg py-2 px-4 font-semibold transition  focus-visible-outline-2 focus-visible:outline-offset-4 focus-visible:outline-neutral-950 focus-visible:rounded-lg hover:bg-neutral-800">View Projects</a>
-            <a href="#contact" className="inline-flex justify-center items-center border border-neutral-300 bg-neutral-50 text-sm rounded-lg py-2 px-4 font-semibold transition focus-visible-outline-2 focus-visible:outline-offset-4 focus-visible:outline-neutral-950 focus-visible:rounded-lg hover:border-neutral-700">Contact me</a>
+            <a href="#projects" className="inline-flex justify-center items-center bg-neutral-950 text-white text-sm rounded-lg py-2 px-4 font-semibold transition  focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950 focus-visible:rounded-lg hover:bg-neutral-800">View Projects</a>
+            <a href="#contact" className="inline-flex justify-center items-center border border-neutral-300 bg-neutral-50 text-sm rounded-lg py-2 px-4 font-semibold transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950 focus-visible:rounded-lg hover:border-neutral-700">Contact me</a>
           </div>
         </div>
       </Container>
