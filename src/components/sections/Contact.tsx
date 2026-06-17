a28bd842137592925692e58ca0e5f46a9986da07
Luan <leluanvn21@seznam.cz>
2026-06-17 17:02:17 +0200

    update contact section

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -13,16 +13,12 @@ function Contact() {
 
             <p className="mt-5 leading-8 text-neutral-600 max-w-2xl">
               I am open to junior frontend developer opportunities. You can
-              contact me through email, GitHub, or LinkedIn.
+              contact me through Email or LinkedIn.
             </p>
           </div>
 
           <div className="mt-8 flex flex-col gap-3 sm:flex-row">
             <a href="mailto:leluanvn21@seznam.cz"  className="inline-flex justify-center items-center bg-neutral-950 text-white text-sm rounded-lg py-2 px-4 font-semibold transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950 focus-visible:rounded-lg  hover:bg-neutral-800">Email me</a>
-
-            <a target="_blank" rel="noopener noreferrer" href="https://github.com/Luan2118" className="inline-flex justify-center items-center border border-neutral-300 text-sm rounded-lg py-2 px-4 font-semibold transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950 focus-visible:rounded-lg  hover:border-neutral-700">
-              GitHub
-            </a>
 
             <a target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/luan-le-7671b9342/" className="inline-flex justify-center items-center border border-neutral-300 text-sm rounded-lg py-2 px-4 font-semibold transition focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-neutral-950 focus-visible:rounded-lg  hover:border-neutral-700">
               LinkedIn
